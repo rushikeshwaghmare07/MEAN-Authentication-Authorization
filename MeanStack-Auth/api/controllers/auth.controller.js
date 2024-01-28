fe08d763ca5b5ffb32e2079982fd7212d6b01d70
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-01-28 20:54:06 +0530

    Create JWT token

--- MeanStack-Auth/api/controllers/auth.controller.js
+++ MeanStack-Auth/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import Role from "../models/Role.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { CreateError } from "../utils/error.js";
 import { CreateSuccess } from "../utils/success.js";
 
@@ -22,7 +23,10 @@ export const register = async (req, res, next)=> {
 
 export const login = async (req, res, next)=> {
     try {
-        const user = await User.findOne({email: req.body.email});
+        const user = await User.findOne({email: req.body.email})
+        .populate("roles", "role");
+
+        const {roles} = user;
         if(!user) {
             return next(CreateError(404, "User not found!"))
         }
@@ -30,7 +34,17 @@ export const login = async (req, res, next)=> {
         if(!isPasswordCorrect) {
             return next(CreateError(400, "Password is incorrect!"));
         }
-        return next(CreateSuccess(200, "Login success!"));
+        const token = jwt.sign(
+            {id: user.id, isAdmin: user.isAdmin, roles: roles},
+            process.env.JWT_SECRET
+        )
+        res.cookie("access_token", token, {httpOnly: true})
+        .status(200)
+        .json({
+            status: 200,
+            message: "Login Success",
+            data: user
+        })
     } catch (error) {
         return next(CreateError(500, "Something went wrong!"));
     }
